69d4092a3456496149904b06486e4d115f5926ab
DuminduKavinda <dumindujayalath98@gmail.com>
2025-05-09 11:43:53 +0530

    new

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -59,7 +59,7 @@
                             />
                             <div class="popular__data">
                                 <h2 class="popular__price">
-                                    <span>Rs.</span>{{ number_format($travel_package->price,2) }}
+                                    <span>$</span>{{ number_format($travel_package->price,2) }}
                                 </h2>
                                 <h3 class="popular__title">
                                     {{ $travel_package->location}}
