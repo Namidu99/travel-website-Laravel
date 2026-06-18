8b2ae0accd2c0425e1f8aec0195a0b6660501189
Namidu99 <nami.hasalanka@gmail.com>
2026-06-18 22:57:00 +0530

    Add calls to CategorySeeder and TravelPackageSeeder in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,5 +19,7 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call(AdminSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(TravelPackageSeeder::class);
     }
 }
